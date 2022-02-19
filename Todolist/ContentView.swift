cc2b90824839214f53f3983483990401084befdf
gregory-aeari <aeari@icloud.com>
2022-02-20 01:22:36 +0900

    입력 받고 리스트에 추가 하는거 까지 함

--- Todolist/ContentView.swift
+++ Todolist/ContentView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var instring = ""
+    
+    @State private var todothing = ""
+    
+    struct Todolist: Identifiable {
+        let id = UUID()
+        var content: String
+        var checked: Bool
+    }
+    
+    @State private var todolists = [Todolist]()
+    
+    func appendlist() {
+        let addlist = Todolist(content: todothing, checked: false)
+        todolists.append(addlist)
+        todothing = ""
+    }
     
     var body: some View {
         VStack{
-            Text("Hello, world")
-                .padding()
-            TextField("insert", text: $instring)
+            Text("That's your future")
+                .font(.title.bold())
+            List{
+                ForEach(0..<todolists.count, id: \.self) { i in
+                    HStack{
+                        Button(action: {}, label: {Image(systemName: "square")})
+                        Text(todolists[i].content)
+                        Spacer()
+                        Button(action: {}, label: {Image(systemName: "trash")})
+                    }
+                }
+            }
+            HStack{
+                Image(systemName: "square.and.pencil")
+                TextField("future",
+                          text: $todothing,
+                          onCommit: {appendlist()}
+                )
+            }
+            .frame(width: 300, height: 50, alignment: .center)
         }
     }
 }
